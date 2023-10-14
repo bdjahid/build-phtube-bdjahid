23cd63cda9e83a980c6720d980905b5884b84e05
Your Name <you@example.com>
2023-10-14 23:44:46 +0600

    responsive

--- script.js
+++ script.js
@@ -22,17 +22,19 @@ const btnHandle = async (categoryId) => {
     const loadData = data.data;
     console.log(loadData)
     const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML = '';
     loadData.forEach((data) => {
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card bg-base-100 shadow-xl">
-        <figure><img src="${data.thumbnail}" alt="Shoes" /></figure>
-        <div class="flex gap-2">
-            <img class="w-16 rounded-full" src="${data.authors[0].profile_picture}" alt="Shoes" alt="">
+        <figure><img src="${data?.thumbnail}" alt="Shoes" /></figure>
+        <p>${data?.others?.posted_date}</p>
+        <div class="flex gap-5 my-5 ps-2">
+            <img class="w-16 rounded-full" src="${data?.authors[0]?.profile_picture}" alt="Shoes" alt="">
            <div>
            <p>${data.title}</p>
-           <p>${data.authors[0].profile_name} ${data.authors[0].verified}</p>
-           <p>${data.others.views}</p>
+           <p>${data?.authors[0]?.profile_name} ${data?.authors[0]?.verified}</p>
+           <p>${data?.others?.views}</p>
            </div>
         </div>
     </div>
